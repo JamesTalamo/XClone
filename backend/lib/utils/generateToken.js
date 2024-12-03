90344947a0f02b070f6b2ece688033cf8e5e5c5a
James Talamo <jamestalamo4@gmail.com>
2024-12-04 02:51:54 +0800

    1.0.2

--- backend/lib/utils/generateToken.js
+++ backend/lib/utils/generateToken.js
@@ -7,8 +7,8 @@ export const generateTokenAndSetCookie = async (userId, res) => {
 
     res.cookie('jwt', token, {
         maxAge: 15 * 24 * 60 * 60 * 1000,
-        http: true,
-        sameSite: "strict",
+        httpOnly: true,
+        sameSite: "None",
         secure: process.env.NODE_ENV !== 'development'
     })
 }
